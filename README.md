b70fd3ffb8be50e62fe59199acc0ed7f43cbb2f9
hualcosa <hualcosa@gmail.com>
2023-03-26 23:04:42 -0300

    commiting first draft

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # IU_DLBDSOOFPP01
+### install sqlite3 to your system
+#### create virtual environment
+#### add your python bin to PATH variable 
---
+++ habit.py
@@ -0,0 +1,30 @@
+import datetime
+
+
+class Habit(object):
+    def __init__(self, name, periodicity):
+        '''
+        Constructor method. Every habit created needs to have the following parameters:
+        name <str> : the name of the habit
+        periodicity <str> : accepted values are 'd' for daily or 'w' for weekly
+        '''
+        self.name = name
+        self.creation_date = datetime.datetime.today()
+        self.periodicity = periodicity
+        self._completed_in = [] # property to check the 
+        self._streak = None
+        self.names_dictionary = {'d': 'daily', 'w': 'weekly'}
+
+        # defensive programming: checking whether the parameters have acceptable values
+        try:
+            assert self.periodicity in {'d', 'w'}
+        except:
+            raise Exception('incorrect value for periodicity. "d" or "w" are accepted values')
+        try:
+            assert len(self.name) > 0
+        except:
+            raise Exception('Name cannot be an empty string')
+        
+    def __str__(self) -> str:
+        
+        return f'Habit: {self.name}\tPeriodicty: {self.names_dictionary[self.periodicity]}\t Creation date: {self.creation_date.strftime("%Y-%m-%d")}'
